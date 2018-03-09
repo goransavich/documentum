1cdc3c32e2bdac3d7741b15deed534a55ce1a807
goransavich <goran.savich@gmail.com>
2018-03-09 10:13:12 +0100

    Add files via upload

---
+++ resources/views/layouts/services.blade.php
@@ -0,0 +1,43 @@
+<div class="services">
+    <div class="container">
+      <div class="text-center">
+        
+      </div>
+      @if(Auth::user()->hasRole(['administrator', 'pisarnica']))
+      <a href="dashboard/create" class="services-outer">
+      <div class="col-md-3 wow fadeInDown avesome-pic" data-wow-duration="1000ms" data-wow-delay="300ms">
+        <i class="fa fa-plus-square-o"></i>
+        <h3>Dodaj novi dokument</h3>
+        <p>Otvaranje novog predmeta, rezervisanje brojeva.</p>
+      </div>
+      </a>
+      @endif
+      @if(Auth::user()->hasRole(['inspektor', 'nacelnik', 'pisarnica', 'administrator']))
+      <a href="dashboard/search" class="services-outer">
+      <div class="col-md-3 wow fadeInDown avesome-pic" data-wow-duration="1000ms" data-wow-delay="600ms">
+        <i class="fa fa-search"></i>
+        <h3>Pretraga</h3>
+        <p>Pretraga predmeta i dodavanje novih dokumenata u traženi predmet.</p>
+      </div>
+      </a>
+      @endif
+      @if(Auth::user()->hasRole(['inspektor', 'nacelnik', 'pisarnica', 'administrator']))
+      <a href="/reports" class="services-outer">
+      <div class="col-md-3 wow fadeInDown avesome-pic" data-wow-duration="1000ms" data-wow-delay="900ms">
+        <i class="fa fa-file-text-o"></i>
+        <h3>Izveštaji</h3>
+        <p>Izveštaji - arhivirani i nearhivirani predmeti, pretraga po inspektorima, po inspekcijama.</p>
+      </div>
+      </a>
+      @endif
+      @if(Auth::user()->hasRole(['administrator']))
+      <a href="controlpanel/department" class="services-outer">
+      <div class="col-md-3 wow fadeInDown avesome-pic" data-wow-duration="1000ms" data-wow-delay="1200ms">
+        <i class="fa fa-gear"></i>
+        <h3>Šifarnici</h3>
+        <p>Unos novih korisnika, organa, opština, jedinica, klasa.</p>
+      </div>
+      </a>
+      @endif
+    </div>
+  </div>
---
+++ resources/views/layouts/slider.blade.php
@@ -0,0 +1,52 @@
+<div class="slider">
+    <div id="about-slider">
+      <div id="carousel-slider" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        
+
+        <div class="carousel-inner">
+          <div class="item active">
+            <img src="img/slide.jpg" class="img-responsive" alt="">
+
+            <div class="carousel-caption">
+                            
+              <div class="wow fadeInUp" data-wow-offset="0" data-wow-delay="0.4s">
+                
+                <form method="POST" action="">
+                  {{csrf_field()}}
+                  <div class="form-group">
+                    <input type="text" class="form-control home-insert" id="username" name="username" placeholder="Korisničko ime">
+                    
+                  </div>
+                  
+                  <div class="form-group">
+                    <input type="password" class="form-control home-insert" id="password" name="password" placeholder="Lozinka">
+                  
+                   
+                  </div>
+                   <small class="text-danger error-message">{{ $errors->first('message') }}</small>
+                  <div class="form-group">
+                    
+                    <button type="submit" name="submit" class="btn btn-primary btn-lg">Prijava</button>
+                  </div>
+                  
+                </form>
+
+              </div>
+                  
+              <div>
+                  
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
+
+        
+      </div>
+      <!--/#carousel-slider-->
+    </div>
+    <!--/#about-slider-->
+  </div>
+  <!--/#slider-->
